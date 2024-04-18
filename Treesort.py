3fc29b779e42a1e63c14839ceba06a5b2b53b7ad
Chris Vandborg <cvand15@student.sdu.dk>
2024-04-18 20:48:57 +0200

    .
    .

--- Treesort.py
+++ Treesort.py
@@ -24,52 +24,13 @@ import DictBinTree
 
 tree = DictBinTree.DictBinTree()
 
-
-"""
-tree = DictBinTree.DictBinTree()
-numbers = []
-while True:
-    try:
-        myInput = input()
-        if myInput == "":
-            break
-        numbers.append(int(myInput))
-    except EOFError as e:
-        print(e)
-
-for num in numbers:
-    print(num)
-"""
-    #number = int(input())
-    #tree.insert(number)
-
-
-
-    
-print(tree.orderedTraversal)
-
-
-#create a loop for recieving user input
-#recieve user input
-# insert userinput into tree
-# print tree when finished
-
-
-# tree.insert(34)
-# tree.insert(645)
-# tree.insert(-45)
-# tree.insert(1)
-# tree.insert(34)
-# tree.insert(0)
-
-"""
 n = 0
+
 for line in sys.stdin:
-    PQHeap.insert(pq,int(line))
+    tree.insert(int(line))
     n = n+1
 
-print()
-while n > 0:
-    print(PQHeap.extractMin(pq))
-    n = n-1
-"""
+tree_keys_ordered = tree.orderedTraversal()
+
+for key in tree_keys_ordered:
+    print(key)
